9956e0ad3961d5d86a3071d2f5e62e3961055f2c
MOYF-beta <changhao.zhang.24@ucl.ac.uk>
2025-02-12 22:01:02 +0000

    feat: enhance dataset setup in exp_setup_local.py for multiple datasets and improve path handling

--- exps/exp_setup_local.py
+++ exps/exp_setup_local.py
@@ -11,12 +11,8 @@ The setup includes:
 '''
 log_path = './logs'
 platform = 'local' if os.getcwd().startswith('/mnt') else 'cluster'
-if platform == 'local':
-    ds_path = '/mnt/c/Users/14152/ZCH/Dev/datasets/SCARED_Images_Resized'
-    splits_dir = '/mnt/c/Users/14152/ZCH/Dev/datasets/SCARED_Images_Resized/splits'
-else:
-    ds_path = '/cluster/project7/Llava_2024/changhao/datasets/SCARED_Images_Resized'
-    splits_dir = '/cluster/project7/Llava_2024/changhao/datasets/SCARED_Images_Resized/splits'
+ds_base = '/mnt/c/Users/14152/ZCH/Dev/datasets' if platform == 'local' else '/cluster/project7/Llava_2024/changhao/datasets'
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 if device == 'cuda':
     torch.backends.cudnn.benchmark = True
@@ -50,6 +46,8 @@ def get_unique_name(name):
 random_seeds(233)
 
 """SCARED DATASET SETUP"""
+ds_path = os.path.join(ds_base, 'SCARED_Images_Resized')
+splits_dir = os.path.join(ds_base, 'SCARED_Images_Resized', 'splits')
 split_train = os.path.join(ds_path, 'splits/train_files.txt')
 split_val = os.path.join(ds_path, 'splits/val_files.txt')
 split_test = os.path.join(ds_path, 'splits/test_files.txt')
@@ -101,3 +99,109 @@ ds_test_multi_frame = SCAREDRAWDataset(
     img_ext='.png'
 )
 
+if platform == 'cluster':
+    '''C3VD DATASET SETUP'''
+    ds_path = os.path.join(ds_base, 'C3VD_as_SCARED')
+    splits_dir = os.path.join(ds_base, 'C3VD_as_SCARED', 'splits')
+    split_train = os.path.join(ds_path, 'splits/train_files.txt')
+    split_test = os.path.join(ds_path, 'splits/test_files.txt')
+    train_filenames = readlines(split_train)
+    test_filenames = readlines(split_test)
+
+    ds_train_c3vd = SCAREDRAWDataset(
+        data_path=ds_path,
+        filenames=train_filenames,
+        frame_idxs=DefaultOpt.frame_ids,
+        height=DefaultOpt.height,
+        width=DefaultOpt.width,
+        num_scales=4,
+        is_train=True,
+        img_ext='.png'
+    )
+
+    split_test = SCAREDRAWDataset(
+        data_path=ds_path,
+        filenames=split_test,
+        frame_idxs=[0],
+        height=DefaultOpt.height,
+        width=DefaultOpt.width,
+        num_scales=4,
+        is_train=False,
+        img_ext='.png'
+    )
+
+    '''hamlyn dataset setup'''
+    ds_path = os.path.join(ds_base, 'hamlyn_as_SCARED')
+    splits_dir = os.path.join(ds_base, 'hamlyn_as_SCARED', 'splits')
+    split_train = os.path.join(ds_path, 'splits/train_files.txt')
+    train_filenames = readlines(split_train)
+
+    ds_train_hamlyn = SCAREDRAWDataset(
+        data_path=ds_path,
+        filenames=train_filenames,
+        frame_idxs=DefaultOpt.frame_ids,
+        height=DefaultOpt.height,
+        width=DefaultOpt.width,
+        num_scales=4,
+        is_train=True,
+        img_ext='.png'
+    )
+    '''PitVis dataset setup'''
+    ds_path = os.path.join(ds_base, 'PitVis_as_SCARED')
+    splits_dir = os.path.join(ds_base, 'PitVis_as_SCARED', 'splits')
+    split_train = os.path.join(ds_path, 'splits/train_files.txt')
+    split_test = os.path.join(ds_path, 'splits/test_files.txt')
+    train_filenames = readlines(split_train)
+    test_filenames = readlines(split_test)
+
+    ds_train_pitvis = SCAREDRAWDataset(
+        data_path=ds_path,
+        filenames=train_filenames,
+        frame_idxs=DefaultOpt.frame_ids,
+        height=DefaultOpt.height,
+        width=DefaultOpt.width,
+        num_scales=4,
+        is_train=True,
+        img_ext='.png'
+    )
+
+    test_filenames = SCAREDRAWDataset(
+        data_path=ds_path,
+        filenames=test_filenames,
+        frame_idxs=[0],
+        height=DefaultOpt.height,
+        width=DefaultOpt.width,
+        num_scales=4,
+        is_train=False,
+        img_ext='.png'
+    )
+
+    '''SyntheticColon dataset setup'''
+    ds_path = os.path.join(ds_base, 'SyntheticColon_as_SCARED')
+    splits_dir = os.path.join(ds_base, 'SyntheticColon_as_SCARED', 'splits')
+    split_train = os.path.join(ds_path, 'splits/train_files.txt')
+    split_test = os.path.join(ds_path, 'splits/test_files.txt')
+    train_filenames = readlines(split_train)
+    test_filenames = readlines(split_test)
+
+    ds_train_syntheticcolon = SCAREDRAWDataset(
+        data_path=ds_path,
+        filenames=train_filenames,
+        frame_idxs=DefaultOpt.frame_ids,
+        height=DefaultOpt.height,
+        width=DefaultOpt.width,
+        num_scales=4,
+        is_train=True,
+        img_ext='.png'
+    )
+
+    test_filenames = SCAREDRAWDataset(
+        data_path=ds_path,
+        filenames=test_filenames,
+        frame_idxs=[0],
+        height=DefaultOpt.height,
+        width=DefaultOpt.width,
+        num_scales=4,
+        is_train=False,
+        img_ext='.png'
+    )
