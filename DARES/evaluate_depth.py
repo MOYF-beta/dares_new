fix: update depth calculation in trainer and find_best functions for improved accuracy

DARES/evaluate_depth.py
@@ -3,43 +3,28 @@ from __future__ import absolute_import, division, print_function
 import os
 import cv2
 import numpy as np
-
 import torch
-from torch.utils.data import DataLoader
-
-from layers import disp_to_depth
+from layers import disp_to_depth 
 from options import MonodepthOptions
 from exps.exp_setup_local import splits_dir
-cv2.setNumThreads(0)  # This speeds up evaluation 5x on our unix systems (OpenCV 3.3.1)
-
-
-
-
-# Models which were trained with stereo supervision were trained with a nominal
-# baseline of 0.1 units. The KITTI rig has a baseline of 54cm. Therefore,
-# to convert our stereo predictions to real-world scale we multiply our depths by 5.4.
-STEREO_SCALE_FACTOR = 5.4
-
+#cv2.setNumThreads(0)  # This speeds up evaluation 5x on our unix systems (OpenCV 3.3.1)
 
 def compute_errors(gt, pred):
-    """Computation of error metrics between predicted and ground truth depths
+    """Computation of error metrics between predicted and ground truth depths using torch on GPU
     """
-    thresh = np.maximum((gt / pred), (pred / gt))
-    a1 = (thresh < 1.25     ).mean()
-    a2 = (thresh < 1.25 ** 2).mean()
-    a3 = (thresh < 1.25 ** 3).mean()
+    thresh = torch.max((gt / pred), (pred / gt))
+    a1 = (thresh < 1.25).float().mean()
+    a2 = (thresh < 1.25 ** 2).float().mean()
+    a3 = (thresh < 1.25 ** 3).float().mean()
 
     rmse = (gt - pred) ** 2
-    rmse = np.sqrt(rmse.mean())
+    rmse = torch.sqrt(rmse.mean())
+    rmse_log = (torch.log(gt) - torch.log(pred)) ** 2
+    rmse_log = torch.sqrt(rmse_log.mean())
+    abs_rel = torch.mean(torch.abs(gt - pred) / gt)
+    sq_rel = torch.mean(((gt - pred) ** 2) / gt)
 
-    rmse_log = (np.log(gt) - np.log(pred)) ** 2
-    rmse_log = np.sqrt(rmse_log.mean())
-
-    abs_rel = np.mean(np.abs(gt - pred) / gt)
-
-    sq_rel = np.mean(((gt - pred) ** 2) / gt)
-
-    return abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3
+    return abs_rel.item(), sq_rel.item(), rmse.item(), rmse_log.item(), a1.item(), a2.item(), a3.item()
 
 
 def batch_post_process_disparity(l_disp, r_disp):
@@ -53,148 +38,70 @@ def batch_post_process_disparity(l_disp, r_disp):
     return r_mask * l_disp + l_mask * r_disp + (1.0 - l_mask - r_mask) * m_disp
 
 
-def evaluate(opt, ds_and_model = {}, frames_input = [0]):
+def evaluate(opt, ds_and_model = {}, frames_input = [0], load_depth_from_npz = False):
     """Evaluates a pretrained model using a specified test set
     """
-    MIN_DEPTH = 1e-3
-    MAX_DEPTH = 150
-
-    if opt.ext_disp_to_eval is None:
-        
-        dataloader = ds_and_model['dataloader']
-        depth_model = ds_and_model['depth_model']
-
-        pred_disps = []
-
-        print("-> Computing predictions with size {}x{}".format(
-            320, 256))
-
-        with torch.no_grad():
-            num_input_frames = len(frames_input)
-            for data in dataloader:
-                if num_input_frames == 1:
-                    input_color = data[("color", frames_input[0], 0)].cuda()
-                else:
-                    input_color = torch.cat([data[("color_aug", i, 0)] for i in frames_input], 1)
-                    batch_size, num_channels, height, width = input_color.shape
-                    input_color = input_color.view(batch_size, num_input_frames, num_channels // num_input_frames, height, width).permute(0, 2, 1, 3, 4)
-                    input_color = input_color.cuda()
-                if opt.post_process:
-                    # Post-processed results require each image to have two forward passes
-                    input_color = torch.cat((input_color, torch.flip(input_color, [3])), 0)
-
-                output = depth_model(input_color)
-                pred_disp, _ = disp_to_depth(output[("disp", 0)], opt.min_depth, opt.max_depth)
-                pred_disp = pred_disp.cpu()[:, 0].numpy()
-
-                if opt.post_process:
-                    N = pred_disp.shape[0] // 2
-                    pred_disp = batch_post_process_disparity(pred_disp[:N], pred_disp[N:, :, ::-1])
-
-                pred_disps.append(pred_disp)
-
-        pred_disps = np.concatenate(pred_disps)
-
-    else:
-        # Load predictions from file
-        print("-> Loading predictions from {}".format(opt.ext_disp_to_eval))
-        pred_disps = np.load(opt.ext_disp_to_eval)
-
-        if opt.eval_eigen_to_benchmark:
-            eigen_to_benchmark_ids = np.load(
-                os.path.join(splits_dir, "benchmark", "eigen_to_benchmark_ids.npy"))
-
-            pred_disps = pred_disps[eigen_to_benchmark_ids]
-
-    if opt.save_pred_disps:
-        output_path = os.path.join(
-            opt.load_weights_folder, "disps_{}_split.npy".format(opt.eval_split))
-        print("-> Saving predicted disparities to ", output_path)
-        np.save(output_path, pred_disps)
-
-    if opt.no_eval:
-        print("-> Evaluation disabled. Done.")
-        quit()
-
-    elif opt.eval_split == 'benchmark':
-        save_dir = os.path.join(opt.load_weights_folder, "benchmark_predictions")
-        print("-> Saving out benchmark predictions to {}".format(save_dir))
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-
-        for idx in range(len(pred_disps)):
-            disp_resized = cv2.resize(pred_disps[idx], (1216, 352))
-            depth = STEREO_SCALE_FACTOR / disp_resized
-            depth = np.clip(depth, 0, 80)
-            depth = np.uint16(depth * 256)
-            save_path = os.path.join(save_dir, "{:010d}.png".format(idx))
-            cv2.imwrite(save_path, depth)
-
-        print("-> No ground truth is available for the KITTI benchmark, so not evaluating. Done.")
-        quit()
-
-    gt_path = os.path.join(splits_dir, opt.eval_split, "gt_depths.npz")
-
-    gt_depths = np.load(gt_path, fix_imports=True, encoding='latin1')["data"]
-
-    print("-> Evaluating")
-
-    if opt.eval_stereo:
-        print("   Stereo evaluation - "
-              "disabling median scaling, scaling by {}".format(STEREO_SCALE_FACTOR))
-        opt.disable_median_scaling = True
-        opt.pred_depth_scale_factor = STEREO_SCALE_FACTOR
-    else:
-        print("   Mono evaluation - using median scaling")
+    dataloader = ds_and_model['dataloader']
+    depth_model = ds_and_model['depth_model']
 
     errors = []
     ratios = []
 
-    for i in range(pred_disps.shape[0]):
+    with torch.no_grad():
+        num_input_frames = len(frames_input)
+        for data in dataloader:
+            if num_input_frames == 1:
+                input_color = data[("color", frames_input[0], 0)].cuda()
+            else:
+                input_color = torch.cat([data[("color_aug", i, 0)] for i in frames_input], 1)
+                batch_size, num_channels, height, width = input_color.shape
+                input_color = input_color.view(batch_size, num_input_frames, num_channels // num_input_frames, height, width).permute(0, 2, 1, 3, 4)
+                input_color = input_color.cuda()
+            
+            if opt.post_process:
+                # Post-processed results require each image to have two forward passes
+                input_color = torch.cat((input_color, torch.flip(input_color, [3])), 0)
 
-        gt_depth = gt_depths[i]
-        gt_height, gt_width = gt_depth.shape[:2]
+            output = depth_model(input_color)
+            min_depth = data['min_depth'].cuda()
+            max_depth = data['max_depth'].cuda()
+            pred_disp, _ = disp_to_depth(output[("disp", 0)], min_depth, max_depth)
+            pred_disp = pred_disp[:, 0]
 
-        pred_disp = pred_disps[i]
-        pred_disp = cv2.resize(pred_disp, (gt_width, gt_height))
-        pred_depth = 1/pred_disp
+            if opt.post_process:
+                N = pred_disp.shape[0] // 2
+                pred_disp = batch_post_process_disparity(pred_disp[:N], pred_disp[N:, :, ::-1])
 
-        if opt.eval_split == "eigen":
-            mask = np.logical_and(gt_depth > MIN_DEPTH, gt_depth < MAX_DEPTH)
+            gt_depth = data["depth_gt"].cuda()
+            gt_height, gt_width = gt_depth.shape[-2:]
 
-            crop = np.array([0.40810811 * gt_height, 0.99189189 * gt_height,
-                             0.03594771 * gt_width,  0.96405229 * gt_width]).astype(np.int32)
-            crop_mask = np.zeros(mask.shape)
-            crop_mask[crop[0]:crop[1], crop[2]:crop[3]] = 1
-            mask = np.logical_and(mask, crop_mask)
+            pred_disp = torch.stack([torch.nn.functional.interpolate(d.unsqueeze(0).unsqueeze(0), size=(gt_height, gt_width), mode='bilinear', align_corners=False).squeeze() for d in pred_disp])
+            pred_depth = 1 / pred_disp
 
-        else:
-            mask = np.logical_and(gt_depth > MIN_DEPTH, gt_depth < MAX_DEPTH)
+            mask = (gt_depth > min_depth[0]) & (gt_depth < max_depth[0])
+            gt_depth = gt_depth[mask]
+            if len(mask.shape) == 4:
+                mask = mask.squeeze(1)
+            pred_depth = pred_depth[mask]
 
-        pred_depth = pred_depth[mask]
-        gt_depth = gt_depth[mask]
+            pred_depth *= opt.pred_depth_scale_factor
+            if not opt.disable_median_scaling:
+                ratio = torch.median(gt_depth) / torch.median(pred_depth)
+                ratios.append(ratio)
+                pred_depth *= ratio
 
-        pred_depth *= opt.pred_depth_scale_factor
-        if not opt.disable_median_scaling:
-            ratio = np.median(gt_depth) / np.median(pred_depth)
-            ratios.append(ratio)
-            pred_depth *= ratio
+            pred_depth = torch.clamp(pred_depth, min=min_depth[0], max=max_depth[0])
 
-        pred_depth[pred_depth < MIN_DEPTH] = MIN_DEPTH
-        pred_depth[pred_depth > MAX_DEPTH] = MAX_DEPTH
-
-        errors.append(compute_errors(gt_depth, pred_depth))
+            errors.append(compute_errors(gt_depth, pred_depth))
 
     if not opt.disable_median_scaling:
-        ratios = np.array(ratios)
-        med = np.median(ratios)
-        print(" Scaling ratios | med: {:0.3f} | std: {:0.3f}".format(med, np.std(ratios / med)))
+        ratios = torch.tensor(ratios)
+        med = torch.median(ratios)
 
-    mean_errors = np.array(errors).mean(0)
+    mean_errors = torch.tensor(errors).mean(0)
 
-    print("\n  " + ("{:>8} | " * 7).format("abs_rel", "sq_rel", "rmse", "rmse_log", "a1", "a2", "a3"))
-    print(("&{: 8.3f}  " * 7).format(*mean_errors.tolist()) + "\\\\")
-    print("\n-> Done!")
+    print(("&{: 8.3f}  " * 7).format(*mean_errors.tolist()) + "\\\\", end='')
+
     return mean_errors
 
 

DARES/layers.py
@@ -16,11 +16,17 @@ def disp_to_depth(disp, min_depth, max_depth):
     The formula for this conversion is given in the 'additional considerations'
     section of the paper.
     """
-    min_disp = 1 / max_depth
-    max_disp = 1 / min_depth
-    scaled_disp = min_disp + (max_disp - min_disp) * disp
-    depth = 1 / scaled_disp
-    return scaled_disp, depth
+    scaled_disps = torch.zeros_like(disp)
+    depths = torch.zeros_like(disp)
+    for i in range(disp.shape[0]):
+        disp[i] = min_depth[i] + (max_depth[i] - min_depth[i]) * disp[i]
+        min_disp = 1 / max_depth[i]
+        max_disp = 1 / min_depth[i]
+        scaled_disp = min_disp + (max_disp - min_disp) * disp[i]
+        depth = 1 / scaled_disp
+        scaled_disps[i] = scaled_disp
+        depths[i] = depth
+    return scaled_disps, depths
 
 
 def transformation_from_parameters(axisangle, translation, invert=False):

exps/dataset.py
@@ -3,32 +3,42 @@ import os
 import random
 import numpy as np
 from PIL import Image 
-from PIL import ImageFile
 import cv2
 import torch
 import torch.utils.data as data
 from torchvision import transforms
+from prefetch_generator import BackgroundGenerator
+USE_PREFETCH = False
 
-ImageFile.LOAD_TRUNCATED_IMAGES=True
-
-def pil_loader(path):
+def pil_loader(path, mode='RGB'):
     with open(path, 'rb') as f:
         with Image.open(f) as img:
-            return img.convert('RGB')
+            if mode == 'RGB':
+                return img.convert('RGB')
+            elif mode == 'GRAY':
+                return img.convert('L')
+            else:
+                raise ValueError("Unsupported mode: {}".format(mode))
+
+def opencv_loader(path, mode = 'RGB'):
+    if mode == 'RGB':
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    elif mode == 'GRAY':
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    else:
+        raise ValueError("Unsupported mode: {}".format(mode))
+    return Image.fromarray(img)
+
+if USE_PREFETCH:
+    class DataLoaderX(torch.utils.data.DataLoader):
+        def __iter__(self):
+            return BackgroundGenerator(super().__iter__(), max_prefetch=32)
+else:
+    DataLoaderX = torch.utils.data.DataLoader
 
 class MonoDataset(data.Dataset):
-    """Superclass for monocular dataloaders
-
-    Args:
-        data_path
-        filenames
-        height
-        width
-        frame_idxs
-        num_scales
-        is_train
-        img_ext
-    """
+    """Superclass for monocular dataloaders with prefetch support"""
     def __init__(self,
                  data_path,
                  filenames,
@@ -36,6 +46,8 @@ class MonoDataset(data.Dataset):
                  width,
                  frame_idxs,
                  num_scales,
+                 min_depth=1e-3,
+                 max_depth=150,
                  is_train=False,
                  img_ext='.png'):
         super(MonoDataset, self).__init__()
@@ -45,36 +57,38 @@ class MonoDataset(data.Dataset):
         self.height = height
         self.width = width
         self.num_scales = num_scales
-        # self.interp = Image.ANTIALIAS
         self.interp = Image.LANCZOS
-
         self.frame_idxs = frame_idxs
-
         self.is_train = is_train
         self.img_ext = img_ext
-
         self.loader = pil_loader
         self.to_tensor = transforms.ToTensor()
+        self.min_depth = min_depth
+        self.max_depth = max_depth
 
-        # We need to specify augmentations differently in newer versions of torchvision.
-        # We first try the newer tuple version; if this fails we fall back to scalars
+        # Color augmentation parameters
         try:
             self.brightness = (0.8, 1.2)
             self.contrast = (0.8, 1.2)
             self.saturation = (0.8, 1.2)
             self.hue = (-0.1, 0.1)
-            transforms.transforms.ColorJitter(self.brightness,self.contrast,self.saturation,self.hue)
+            transforms.transforms.ColorJitter(
+                self.brightness, self.contrast, self.saturation, self.hue)
         except TypeError:
             self.brightness = 0.2
             self.contrast = 0.2
             self.saturation = 0.2
             self.hue = 0.1
 
+        # Initialize resize transforms
         self.resize = {}
         for i in range(self.num_scales):
             s = 2 ** i
-            self.resize[i] = transforms.Resize((self.height // s, self.width // s),
-                                               interpolation=self.interp)
+            self.resize[i] = transforms.Resize(
+                (self.height // s, self.width // s),
+                interpolation=self.interp
+            )
+        
         self.load_depth = self.check_depth()
 
     def preprocess(self, inputs, color_aug):
@@ -97,6 +111,11 @@ class MonoDataset(data.Dataset):
                 n, im, i = k
                 inputs[(n, im, i)] = self.to_tensor(f)
                 inputs[(n + "_aug", im, i)] = self.to_tensor(color_aug(f))
+
+    def preprocess_depth(self, inputs):
+        if "depth_gt" in inputs.keys():
+            depth_gt = inputs["depth_gt"]
+            inputs["depth_gt"] = self.resize[0](depth_gt)
 
     def __len__(self):
         return len(self.filenames)
@@ -137,7 +156,9 @@ class MonoDataset(data.Dataset):
 
         inputs["sequence"] = torch.from_numpy(np.array(int(sequence)))
         inputs["keyframe"] = torch.from_numpy(np.array(int(keyframe)))
-
+        
+        inputs["min_depth"] = torch.from_numpy(np.array(self.min_depth))
+        inputs["max_depth"] = torch.from_numpy(np.array(self.max_depth))
         if len(line) == 3:
             frame_index = int(line[1])
         else:
@@ -179,9 +200,14 @@ class MonoDataset(data.Dataset):
             del inputs[("color_aug", i, -1)]
 
         if self.load_depth:
-            depth_gt = self.get_depth(folder, frame_index, side, do_flip)
-            inputs["depth_gt"] = np.expand_dims(depth_gt, 0)
-            inputs["depth_gt"] = torch.from_numpy(inputs["depth_gt"].astype(np.float32))
+            try:
+                depth_gt = self.get_depth(folder, frame_index, side, do_flip)
+                self.preprocess_depth(inputs)
+                inputs["depth_gt"] = np.expand_dims(depth_gt, 0)
+                inputs["depth_gt"] = torch.from_numpy(inputs["depth_gt"])
+            except FileNotFoundError:
+                print(f'Warning: missing depth map for {folder} {frame_index} {side}')
+                pass
 
         if "s" in self.frame_idxs:
             stereo_T = np.eye(4, dtype=np.float32)
@@ -201,21 +227,21 @@ class MonoDataset(data.Dataset):
     def get_depth(self, folder, frame_index, side, do_flip):
         raise NotImplementedError
 
-class SCAREDDataset(MonoDataset):
-    def __init__(self, *args, **kwargs):
-        super(SCAREDDataset, self).__init__(*args, **kwargs)
-
+class SCAREDRAWDataset(MonoDataset):
+    def __init__(self, *args, load_depth=False, depth_rescale_factor=1, **kwargs):
+        self.load_depth = load_depth
+        self.depth_rescale_factor = depth_rescale_factor
+        super(SCAREDRAWDataset, self).__init__(*args, **kwargs)
         self.K = np.array([[0.82, 0, 0.5, 0],
-                           [0, 1.02, 0.5, 0],
-                           [0, 0, 1, 0],
-                           [0, 0, 0, 1]], dtype=np.float32)
-
-        # self.full_res_shape = (1280, 1024)
+                          [0, 1.02, 0.5, 0],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]], dtype=np.float32)
         self.side_map = {"2": 2, "3": 3, "l": 2, "r": 3}
+        
+        
 
     def check_depth(self):
-        
-        return False
+        return self.load_depth
 
     def get_color(self, folder, frame_index, side, do_flip):
         image_path = self.get_image_path(folder, frame_index, side)
@@ -230,41 +256,31 @@ class SCAREDDataset(MonoDataset):
 
         return color
 
-
-class SCAREDRAWDataset(SCAREDDataset):
-    def __init__(self, *args, **kwargs):
-        super(SCAREDRAWDataset, self).__init__(*args, **kwargs)
-
     def get_image_path(self, folder, frame_index, side):
         f_str = "{:010d}{}".format(frame_index, self.img_ext)
         image_path = os.path.join(
             self.data_path, folder, "image_0{}/data".format(self.side_map[side]), f_str)
 
         return image_path
-
+    
     def get_depth(self, folder, frame_index, side, do_flip):
-        f_str = "scene_points{:06d}.tiff".format(frame_index-1)
-
+        f_str = "{:010d}{}".format(frame_index, self.img_ext)
         depth_path = os.path.join(
-            self.data_path,
-            folder,
-            "image_0{}/data/groundtruth".format(self.side_map[side]),
-            f_str)
+            self.data_path, folder, "depth_0{}/data".format(self.side_map[side]), f_str)
 
-        depth_gt = cv2.imread(depth_path, 3)
-        depth_gt = depth_gt[:, :, 0]
-        depth_gt = depth_gt[0:1024, :]
+        depth_gt = self.loader(depth_path, mode='GRAY')
+        depth_gt = np.array(depth_gt).astype(np.float32)
         if do_flip:
             depth_gt = np.fliplr(depth_gt)
 
-        return depth_gt
+        return depth_gt * self.depth_rescale_factor
 
     
 
 if __name__ == "__main__":
 
-    fpath = 'C:\\Users\\14152\\ZCH\\Dev\\datasets\\SCARED_Images_Resized'
-    split_file = os.path.join(fpath, 'splits\\train_files.txt')
+    fpath = '/mnt/c/Users/14152/ZCH/Dev/datasets/C3VD_as_SCARED'
+    split_file = '/mnt/c/Users/14152/ZCH/Dev/datasets/all_splits/c3vd/train_files.txt'
     ds_filenames = [line.rstrip() for line in open(split_file).readlines()]
     dataset = SCAREDRAWDataset(
         data_path=fpath, # file path
@@ -274,8 +290,27 @@ if __name__ == "__main__":
         width=320,
         num_scales=4, # rescale factor for image pyramid
         is_train=True,
-        img_ext='.png'
+        img_ext='.png',
+        load_depth=True,
+        depth_rescale_factor=100 / 65535
     )
 
     print(len(dataset))
     print(dataset[0].keys())
+
+    import matplotlib.pyplot as plt
+
+    # Display the color image
+    color_image = dataset[0][('color', 0, 0)].permute(1, 2, 0).numpy()
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.title("Color Image")
+    plt.imshow(color_image)
+
+    # Display the depth map
+    depth_map = dataset[0]['depth_gt'].squeeze().numpy()
+    plt.subplot(1, 2, 2)
+    plt.title("Depth Map")
+    plt.imshow(depth_map, cmap='gray')
+
+    plt.show()

exps/exp_setup_local.py
@@ -63,6 +63,8 @@ ds_train = SCAREDRAWDataset(
     frame_idxs=DefaultOpt.frame_ids,
     height=DefaultOpt.height,
     width=DefaultOpt.width,
+    min_depth=0.1,
+    max_depth=150,
     num_scales=4,
     is_train=True,
     img_ext='.png'
@@ -74,6 +76,8 @@ ds_val = SCAREDRAWDataset(
     frame_idxs=DefaultOpt.frame_ids,
     height=DefaultOpt.height,
     width=DefaultOpt.width,
+    min_depth=0.1,
+    max_depth=150,
     num_scales=4,
     is_train=False,
     img_ext='.png'
@@ -85,7 +89,9 @@ ds_test = SCAREDRAWDataset(
     frame_idxs=[0],
     height=DefaultOpt.height,
     width=DefaultOpt.width,
-    num_scales=4,
+    min_depth=0.1,
+    max_depth=150,
+    num_scales=1,
     is_train=False,
     img_ext='.png'
 )
@@ -96,148 +102,179 @@ ds_test_multi_frame = SCAREDRAWDataset(
     frame_idxs=DefaultOpt.frame_ids,
     height=DefaultOpt.height,
     width=DefaultOpt.width,
-    num_scales=4,
+    min_depth=0.1,
+    max_depth=150,
+    num_scales=1,
     is_train=False,
     img_ext='.png'
 )
 
-if platform == 'cluster':
-    '''C3VD DATASET SETUP'''
-    ds_path = os.path.join(ds_base, 'C3VD_as_SCARED')
-    splits_dir = os.path.join(ds_base, 'C3VD_as_SCARED', 'splits')
-    split_train = os.path.join(ds_path, 'splits/train_files.txt')
-    split_test = os.path.join(ds_path, 'splits/test_files.txt')
-    train_filenames = readlines(split_train)
-    test_filenames = readlines(split_test)
 
-    ds_train_c3vd = SCAREDRAWDataset(
-        data_path=ds_path,
-        filenames=train_filenames,
-        frame_idxs=DefaultOpt.frame_ids,
-        height=DefaultOpt.height,
-        width=DefaultOpt.width,
-        num_scales=4,
-        is_train=True,
-        img_ext='.png'
-    )
+'''C3VD DATASET SETUP'''
+ds_path = os.path.join(ds_base, 'C3VD_as_SCARED')
+splits_dir = os.path.join(ds_base, 'C3VD_as_SCARED', 'splits')
+split_train = os.path.join(ds_path, 'splits/train_files.txt')
+split_test = os.path.join(ds_path, 'splits/test_files.txt')
+train_filenames = readlines(split_train)
+test_filenames = readlines(split_test)
+ds_train_c3vd = SCAREDRAWDataset(
+    data_path=ds_path,
+    filenames=train_filenames,
+    frame_idxs=DefaultOpt.frame_ids,
+    height=DefaultOpt.height,
+    width=DefaultOpt.width,
+    min_depth=0.1,
+    max_depth=100,
+    num_scales=4,
+    is_train=True,
+    img_ext='.png'
+)
+ds_test_c3vd = SCAREDRAWDataset(
+    data_path=ds_path,
+    filenames=test_filenames,
+    frame_idxs=[0],
+    height=DefaultOpt.height,
+    width=DefaultOpt.width,
+    num_scales=1,
+    depth_rescale_factor = 100 / 65535,
+    min_depth=0.1,
+    max_depth=100,
+    is_train=False,
+    load_depth=True,
+    img_ext='.png'
+)
+'''hamlyn dataset setup'''
+ds_path = os.path.join(ds_base, 'hamlyn_as_SCARED')
+splits_dir = os.path.join(ds_base, 'hamlyn_as_SCARED', 'splits')
+split_train = os.path.join(ds_path, 'splits/train_files.txt')
+split_test = os.path.join(ds_path, 'splits/test_files.txt')
+train_filenames = readlines(split_train)
+test_filenames = readlines(split_test)
 
-    ds_test_c3vd = SCAREDRAWDataset(
-        data_path=ds_path,
-        filenames=split_test,
-        frame_idxs=[0],
-        height=DefaultOpt.height,
-        width=DefaultOpt.width,
-        num_scales=4,
-        is_train=False,
-        img_ext='.png'
-    )
+hamlyn_ratio = 0.3
+ds_train_hamlyn = SCAREDRAWDataset(
+    data_path=ds_path,
+    filenames=train_filenames[:int(hamlyn_ratio * len(train_filenames))],
+    frame_idxs=DefaultOpt.frame_ids,
+    height=DefaultOpt.height,
+    width=DefaultOpt.width,
+    num_scales=4,
+    min_depth=0.1,
+    max_depth=150,
+    is_train=True,
+    img_ext='.png'
+)
 
-    '''hamlyn dataset setup'''
-    ds_path = os.path.join(ds_base, 'hamlyn_as_SCARED')
-    splits_dir = os.path.join(ds_base, 'hamlyn_as_SCARED', 'splits')
-    split_train = os.path.join(ds_path, 'splits/train_files.txt')
-    train_filenames = readlines(split_train)
-    
-    hamlyn_ratio = 0.3
-    ds_train_hamlyn = SCAREDRAWDataset(
-        data_path=ds_path,
-        filenames=train_filenames[:int(hamlyn_ratio * len(train_filenames))],
-        frame_idxs=DefaultOpt.frame_ids,
-        height=DefaultOpt.height,
-        width=DefaultOpt.width,
-        num_scales=4,
-        is_train=True,
-        img_ext='.png'
-    )
-    '''PitVis dataset setup'''
-    ds_path = os.path.join(ds_base, 'PitVis_as_SCARED')
-    splits_dir = os.path.join(ds_base, 'PitVis_as_SCARED', 'splits')
-    split_train = os.path.join(ds_path, 'splits/train_files.txt')
-    split_test = os.path.join(ds_path, 'splits/test_files.txt')
-    train_filenames = readlines(split_train)
-    test_filenames = readlines(split_test)
+ds_test_hamlyn = SCAREDRAWDataset(
+    data_path=ds_path,
+    filenames=test_filenames,
+    frame_idxs=[0],
+    height=DefaultOpt.height,
+    width=DefaultOpt.width,
+    load_depth=True,
+    depth_rescale_factor= 1,
+    num_scales=1,
+    min_depth=0.1,
+    max_depth=150,
+    is_train=False,
+    img_ext='.png'
+)
 
-    ds_train_pitvis = SCAREDRAWDataset(
-        data_path=ds_path,
-        filenames=train_filenames,
-        frame_idxs=DefaultOpt.frame_ids,
-        height=DefaultOpt.height,
-        width=DefaultOpt.width,
-        num_scales=4,
-        is_train=True,
-        img_ext='.png'
-    )
-
-    ds_test_pitvis = SCAREDRAWDataset(
-        data_path=ds_path,
-        filenames=test_filenames,
-        frame_idxs=[0],
-        height=DefaultOpt.height,
-        width=DefaultOpt.width,
-        num_scales=4,
-        is_train=False,
-        img_ext='.png'
-    )
-
-    '''SyntheticColon dataset setup'''
-    ds_path = os.path.join(ds_base, 'SyntheticColon_as_SCARED')
-    splits_dir = os.path.join(ds_base, 'SyntheticColon_as_SCARED', 'splits')
-    split_train = os.path.join(ds_path, 'splits/train_files.txt')
-    split_test = os.path.join(ds_path, 'splits/test_files.txt')
-    train_filenames = readlines(split_train)
-    test_filenames = readlines(split_test)
-
-    ds_train_syntheticcolon = SCAREDRAWDataset(
-        data_path=ds_path,
-        filenames=train_filenames,
-        frame_idxs=DefaultOpt.frame_ids,
-        height=DefaultOpt.height,
-        width=DefaultOpt.width,
-        num_scales=4,
-        is_train=True,
-        img_ext='.png'
-    )
-
-    ds_test_syntheticcolon = SCAREDRAWDataset(
-        data_path=ds_path,
-        filenames=test_filenames,
-        frame_idxs=[0],
-        height=DefaultOpt.height,
-        width=DefaultOpt.width,
-        num_scales=4,
-        is_train=False,
-        img_ext='.png'
-    )
-
-    ds_base_model_train = torch.utils.data.ConcatDataset([ds_train, ds_train_c3vd, ds_train_hamlyn, ds_train_syntheticcolon])
+'''PitVis dataset setup'''
+ds_path = os.path.join(ds_base, 'PitVis_as_SCARED')
+splits_dir = os.path.join(ds_base, 'PitVis_as_SCARED', 'splits')
+split_train = os.path.join(ds_path, 'splits/train_files.txt')
+split_test = os.path.join(ds_path, 'splits/test_files.txt')
+train_filenames = readlines(split_train)
+test_filenames = readlines(split_test)
+ds_train_pitvis = SCAREDRAWDataset(
+    data_path=ds_path,
+    filenames=train_filenames,
+    frame_idxs=DefaultOpt.frame_ids,
+    height=DefaultOpt.height,
+    width=DefaultOpt.width,
+    num_scales=4,
+    is_train=True,
+    img_ext='.png'
+)
+ds_test_pitvis = SCAREDRAWDataset(
+    data_path=ds_path,
+    filenames=test_filenames,
+    frame_idxs=[0],
+    height=DefaultOpt.height,
+    width=DefaultOpt.width,
+    num_scales=1,
+    is_train=False,
+    img_ext='.png'
+)
+'''SyntheticColon dataset setup'''
+ds_path = os.path.join(ds_base, 'SyntheticColon_as_SCARED')
+splits_dir = os.path.join(ds_base, 'SyntheticColon_as_SCARED', 'splits')
+split_train = os.path.join(ds_path, 'splits/train_files.txt')
+split_test = os.path.join(ds_path, 'splits/test_files.txt')
+train_filenames = readlines(split_train)
+test_filenames = readlines(split_test)
+ds_train_syntheticcolon = SCAREDRAWDataset(
+    data_path=ds_path,
+    filenames=train_filenames,
+    frame_idxs=DefaultOpt.frame_ids,
+    height=DefaultOpt.height,
+    width=DefaultOpt.width,
+    min_depth=0.1,
+    max_depth=20,
+    num_scales=4,
+    is_train=True,
+    img_ext='.png'
+)
+ds_test_syntheticcolon = SCAREDRAWDataset(
+    data_path=ds_path,
+    filenames=test_filenames,
+    frame_idxs=[0],
+    height=DefaultOpt.height,
+    width=DefaultOpt.width,
+    num_scales=1,
+    is_train=False,
+    load_depth=True,
+    min_depth=0.1,
+    max_depth=20,
+    depth_rescale_factor= 20 / 65535,
+    img_ext='.png'
+)
+ds_base_model_train = torch.utils.data.ConcatDataset([ds_train, ds_train_c3vd, ds_train_hamlyn, ds_train_syntheticcolon])
 
 def check_ds():
     from tqdm import tqdm
+    from concurrent.futures import ThreadPoolExecutor
     print(f"Train: {len(ds_train)}")
     print(f"Val: {len(ds_val)}")
     print(f"Test: {len(ds_test)}")
     print(f"Test Multi Frame: {len(ds_test_multi_frame)}")
-    if platform == 'cluster':
-        print(f"Train C3VD: {len(ds_train_c3vd)}")
-        print(f"Test C3VD: {len(ds_test_c3vd)}")
-        print(f"Train Hamlyn: {len(ds_train_hamlyn)}")
-        print(f"Train PitVis: {len(ds_train_pitvis)}")
-        print(f"Test PitVis: {len(ds_test_pitvis)}")
-        print(f"Train SyntheticColon: {len(ds_train_syntheticcolon)}")
-        print(f"Test SyntheticColon: {len(ds_test_syntheticcolon)}")
-    ds_to_check = [ds_train, ds_val, ds_test, ds_test_multi_frame]
-    if platform == 'cluster':
-        ds_to_check += [ds_train_c3vd, ds_test_c3vd, ds_train_hamlyn, ds_train_pitvis, ds_test_pitvis, ds_train_syntheticcolon, ds_test_syntheticcolon]
-
+    print(f"Train C3VD: {len(ds_train_c3vd)}")
+    print(f"Test C3VD: {len(ds_test_c3vd)}")
+    print(f"Train Hamlyn: {len(ds_train_hamlyn)}")
+    print(f"Test Hamlyn: {len(ds_test_hamlyn)}")
+    print(f"Train PitVis: {len(ds_train_pitvis)}")
+    print(f"Test PitVis: {len(ds_test_pitvis)}")
+    print(f"Train SyntheticColon: {len(ds_train_syntheticcolon)}")
+    print(f"Test SyntheticColon: {len(ds_test_syntheticcolon)}")
+    ds_to_check = [ds_train_c3vd, ds_test_c3vd, ds_train_hamlyn, ds_test_hamlyn, ds_train_syntheticcolon, ds_test_syntheticcolon]
     for ds in ds_to_check:
         for i in tqdm(range(len(ds)), desc=f"Checking dataset {ds}"):
             try:
-                a = ds[i]
+                def load_data(index):
+                    try:
+                        return ds[index]
+                    except Exception as e:
+                        print(f"Error at {index}: {e}")
+                        return None
+
+                with ThreadPoolExecutor() as executor:
+                    list(tqdm(executor.map(load_data, range(len(ds))), total=len(ds), desc=f"Checking dataset {ds}"))
             except Exception as e:
                 print(f"Error at {i}: {e}")
 
 if __name__ == "__main__":
-    #check_ds()
+    check_ds()
     split_train = "/mnt/c/Users/14152/ZCH/Dev/datasets/C3VD_as_SCARED/splits/test_files.txt"
     a = readlines(split_train)
     print(len(a))

exps/find_best.py
@@ -2,17 +2,20 @@ import os
 import argparse
 import warnings
 import json
-from torch.utils.data import DataLoader
+from exps.dataset import DataLoaderX as DataLoader
 from exps.load_other_models import *
 import shutil
 
 from DARES.evaluate_depth import evaluate
 from exps.options import DotDict
-
-def find_best(model_type, model_name, only_keep_best=False):
+from exps.exp_setup_local import log_path
+def find_best(model_type, model_name, only_keep_best=False, ds_name='SCARED', dataset = None):
     warnings.filterwarnings("ignore")
     if model_type == 'DARES' or model_type == 'DARES_CPE' or model_type == 'DARES_peft':
-        from exps.exp_setup_local import ds_test, ds_test_multi_frame, log_path
+        if dataset is None:
+            from exps.exp_setup_local import ds_test, ds_test_multi_frame
+        else:
+            ds_test = dataset
     else:
         raise ValueError(f"Model type {model_type} not supported.")
     model_path = os.path.join(log_path, model_name, 'models')
@@ -23,48 +26,47 @@ def find_best(model_type, model_name, only_keep_best=False):
     opt_run = json.load(open(os.path.join(model_path, 'opt.json'), 'r'))
     opt_dict = DotDict(opt_run)
     # Helper function to evaluate a model with a specific set of weights
-    def evaluate_model(model_type, weight_path):
-        if model_type == 'DARES':
-            test_dataloader = DataLoader(ds_test, 16, shuffle=False, pin_memory=True, drop_last=False)
+    
+    def evaluate_model(model_type, weight_path, dataset = ds_test, load_depth_from_npz = ds_name=='SCARED'):
         
+        if model_type == 'DARES':
+            test_dataloader = DataLoader(dataset, 16, shuffle=False, pin_memory=True, drop_last=False, num_workers=10)
             depth_model = load_DARES(opt_dict, weight_path, 'depth_model.pth', refine=False)
             ds_and_model = {
                 'dataloader': test_dataloader,
                 'depth_model': depth_model
             }
-            return evaluate(opt_dict, ds_and_model=ds_and_model)
+            return evaluate(opt_dict, ds_and_model=ds_and_model, load_depth_from_npz=load_depth_from_npz)
         elif model_type == 'DARES_peft':
-            test_dataloader = DataLoader(ds_test, 16, shuffle=False, pin_memory=True, drop_last=False)
-        
+            test_dataloader = DataLoader(dataset, 16, shuffle=False, pin_memory=True, drop_last=False, num_workers=10)
             depth_model = load_DARES(opt_dict, weight_path, 'depth_model.pth', refine=False, peft=True)
             ds_and_model = {
                 'dataloader': test_dataloader,
                 'depth_model': depth_model
             }
-            return evaluate(opt_dict, ds_and_model=ds_and_model)        
+            return evaluate(opt_dict, ds_and_model=ds_and_model, load_depth_from_npz=load_depth_from_npz)        
         elif model_type == 'DARES_refine':
-            test_dataloader = DataLoader(ds_test, 16, shuffle=False, pin_memory=True, drop_last=False)
-        
+            test_dataloader = DataLoader(dataset, 16, shuffle=False, pin_memory=True, drop_last=False, num_workers=10)
             depth_model = load_DARES(opt_dict, weight_path, 'depth_model.pth', refine=True)
             ds_and_model = {
                 'dataloader': test_dataloader,
                 'depth_model': depth_model
             }
-            return evaluate(opt_dict, ds_and_model=ds_and_model) 
+            return evaluate(opt_dict, ds_and_model=ds_and_model, load_depth_from_npz=load_depth_from_npz) 
         elif model_type == 'DARES_CPE':
-            test_dataloader = DataLoader(ds_test_multi_frame, 16, shuffle=False, pin_memory=True, drop_last=False)
+            test_dataloader = DataLoader(ds_test_multi_frame, 16, shuffle=False, pin_memory=True, drop_last=False, num_workers=10)
         
             depth_model = load_DARES_CPE(opt_dict, weight_path, 'depth_model.pth')
             ds_and_model = {
                 'dataloader': test_dataloader,
                 'depth_model': depth_model
             }
-            return evaluate(opt_dict, ds_and_model=ds_and_model, frames_input=opt_dict.frame_ids)    
+            return evaluate(opt_dict, ds_and_model=ds_and_model, frames_input=opt_dict.frame_ids, load_depth_from_npz=load_depth_from_npz)    
     
     print(f"Testing {model_type} ({model_name})")
     weights = [w for w in os.listdir(model_path) if w.startswith('weights_')]
     print(f"Found {len(weights)} weights for {model_name}")
-
+    print("\n  " + ("{:>8} | " * 7).format("abs_rel", "sq_rel", "rmse", "rmse_log", "a1", "a2", "a3"))
     best_score = float('inf')
     best_weight = None
 
@@ -82,10 +84,9 @@ def find_best(model_type, model_name, only_keep_best=False):
 
     for weight in weights:
         weight_path = os.path.join(model_path, weight)
-        print(f"Evaluating {model_type} with weights: {weight}")
-
 
         score = evaluate_model(model_type, weight_path)
+        print(f"\t{weight}")
         if score[index] < best_score:
             best_score = score[index]
             best_weight = weight
@@ -112,13 +113,13 @@ def find_best(model_type, model_name, only_keep_best=False):
 
     # Output best weights and scores to a file
     result_file = os.path.join(model_path, 'best_results.txt')
-    with open(result_file, 'w') as f:
-        f.write(f'Best weight path: {best_weight}\n')
+    with open(result_file, 'a') as f:
+        f.write(f'Best weight path: {best_weight} for dataset {ds_name}\n')
         f.write('| Method        | abs_rel | sq_rel | rmse   | rmse_log | a1    | a2    | a3    |\n')
         f.write('|---------------|---------|--------|--------|----------|-------|-------|-------|\n')
         f.write(f'| {model_type:<13} | {best_score:<7.4f} | {score[1]:<7.4f} | {score[2]:<7.4f} | {score[3]:<7.4f} | {score[4]:<7.4f} | {score[5]:<7.4f} | {score[6]:<7.4f} |\n')
 
-    print(f"Results saved to {result_file}")
+    print(f"Results appended to {result_file}")
 
 if __name__ == "__main__":
     # Parse command-line arguments

exps/trainer_abc.py
@@ -4,7 +4,7 @@ import time
 
 import torch
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from exps.dataset import DataLoaderX as DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchmetrics.image import MultiScaleStructuralSimilarityIndexMeasure
 from DARES.layers import *
@@ -318,8 +318,8 @@ class Trainer(ABC):
             else:
                 disp = F.interpolate(
                     disp, [self.opt.height, self.opt.width], mode="bilinear", align_corners=True)
-
-            _, depth = disp_to_depth(disp, self.opt.min_depth, self.opt.max_depth)
+            min_depth, max_depth = inputs["min_depth"], inputs["max_depth"]
+            _, depth = disp_to_depth(disp, min_depth, max_depth)
 
             outputs[("depth", 0, scale)] = depth
 