229c3f98a08ca683deac28319545f741478395e4
MOYF-beta <changhao.zhang.24@ucl.ac.uk>
2025-02-17 19:47:40 +0000

    refactor: rename model variable for clarity and remove debug flag from trainer initialization

--- exps/attn_encoder_dora_base_model/train_attn_encoder_dora_base.py
+++ exps/attn_encoder_dora_base_model/train_attn_encoder_dora_base.py
@@ -10,13 +10,13 @@ from exps.exp_setup_local import (ds_val, check_test_only, get_unique_name, log_
 from exps.find_best import find_best
 
 opt = AttnEncoderOpt
-model_name = 'base_model_attn_encoder_dora'
+model_name = 'base_model_1'
 pretrained_root_dir = './DARES/af_sfmlearner_weights'
 if __name__ == "__main__":
     if not check_test_only():
         trainer = TrainerAttnEncoder(model_name, log_path, opt, 
                           train_eval_ds={'train': ds_base_model_train, 'val': ds_val},
-                          pretrained_root_dir=pretrained_root_dir, debug=True)
+                          pretrained_root_dir=pretrained_root_dir)
         trainer.train()
     find_best('DARES_peft', model_name, dataset=ds_test, dataset_name='SCARED')
     find_best('DARES_peft', model_name, dataset=ds_test_c3vd, dataset_name='C3VD')
